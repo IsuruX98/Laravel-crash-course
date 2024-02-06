0fa1c59554f3212dc6efea36f022cfd5cb354dec
IsuruX98 <isurusanka98@gmail.com>
2024-02-06 11:33:15 +0530

    seeder created

---
+++ database/seeders/ProductTableData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductTableData extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('products')->insert([
+            'name' => "hello",
+            'qty' => 15,
+            'price' => 15.2,
+            'description' => "meka thamai visthare",
+        ]);
+    }
+}
