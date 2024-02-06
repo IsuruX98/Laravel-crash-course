2c48af21c3f445e0d0d90d3da26d953fa4cc5a84
IsuruX98 <isurusanka98@gmail.com>
2024-02-06 17:07:18 +0530

    login created useing breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Dashboard</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
 <body>
-    <div class="container text-center">
+    <div>
+        <div class="container-fluid link-group w-100 py-3 d-flex justify-content-between">
+            <h4 class="font-weight-normal">Laravel Test App</h4>
+            <div><!-- Login and Register Links -->
+                <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+                <a href="{{ route('register') }}" class="btn btn-dark">Register</a>
+            </div>
+        </div>
 
-        <h1 class="mt-3">Livewire 3 tutorial</h1>
+        <div class="container text-center mt-5">
 
-        @livewire('counter')
+            <h1>Livewire 3 Tutorial</h1>
 
+            <!-- Livewire Counter Component -->
+            @livewire('counter')
+
+        </div>
     </div>
 </body>
 
