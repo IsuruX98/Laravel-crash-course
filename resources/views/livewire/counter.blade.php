d19dff96825ddd2d81c28f3903dfc491f7d4ca05
IsuruX98 <isurusanka98@gmail.com>
2024-02-06 14:28:36 +0530

    livewire created

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button class="btn btn-success font-weight-bolder" wire:click="up"> + </button>
+    <span class="mx-2"> {{ $count }} </span>
+    <button class="btn btn-danger font-weight-bolder" wire:click="down"> - </button>
+</div>
